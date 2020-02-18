f82b4778a9e8bd7de72cd2d55470e07fbe20e715
Viper <56606800+vipersniper0501@users.noreply.github.com>
2020-02-18 16:22:25 +0100

    Delete balloon.sh

--- balloon.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-chmod +x /etc/Moore_Flight_Computer/camera.sh #makes the file camera.sh executable
-chmod +x /etc/Moore_Flight_Computer/data.sh #makes the file data.sh executable
-/etc/Moore_Flight_Computer/camera.sh & 
-disown
-/etc/Moore_Flight_Computer/data.sh &
