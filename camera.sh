31715429c54b070ce527f2984bf0b45facfba257
Viper <56606800+vipersniper0501@users.noreply.github.com>
2020-02-18 16:22:33 +0100

    Delete camera.sh

--- camera.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PICSDIR=/etc/Moore_Flight_Coomputer/Pictures  #sends pictures to the folder pictures on Pi
-while true; do #creates 
-	#DATE=`date +%Y%m%d`
-	#HOUR=`date +%H%M%S`
-	raspistill -o $PICSDIR/`date +%s`.jpg
-	sleep 30 #waits 30 seconds before continuing
-done
