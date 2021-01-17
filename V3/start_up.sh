16b35cb1a0ce99dfd3ce16144623a8fc578eea2f
Viper <56606800+vipersniper0501@users.noreply.github.com>
2021-01-17 10:59:04 +0100

    Update start_up.sh

--- V3/start_up.sh
+++ V3/start_up.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-#Starts up the raspberry pi cameras
+# Starts up the raspberry pi cameras
+# Make sure to have a cron job run this @reboot
+# Example of cron setup: @reboot /home/pi/Documents/start_up.sh
 
 screen -d -m sh /home/pi/Documents/take_picture.sh
